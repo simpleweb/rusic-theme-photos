9c6c9231107ffd03101b2d62ad2b7a956a7d6312
Dale Williams <dale@simpleweb.co.uk>
2012-01-31 10:07:12 +0000

    removed masonry settings from scripts file

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -19,39 +19,6 @@ $(document).ready(function(){
         }
       });
       
-      
-      var $optionSets = $('.sort-order'),
-          $optionLinks = $optionSets.find('a.likes, a.popular');
-
-      $optionLinks.click(function(){
-        var $this = $(this);
-        // don't proceed if already selected
-        if ( $this.hasClass('active') ) {
-          return false;
-        }
-        var $optionSet = $this.parents('.sort-order');
-        $optionSet.find('.active').removeClass('active');
-        $this.addClass('active');
-  
-        // make option object dynamically, i.e. { filter: '.my-filter-class' }
-        var options = {},
-            key = $optionSet.attr('data-option-key'),
-            value = $this.attr('data-option-value');
-        // parse 'false' as false boolean
-        value = value === 'false' ? false : value;
-        options[ key ] = value;
-        if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
-          // changes in layout modes need extra logic
-          changeLayoutMode( $this, options )
-        } else {
-          // otherwise, apply new options
-          $container.isotope( options );
-        }
-        
-        return false;
-      });
-
-      
     });
 
     $('.alert-message.prompt').delay(2500).fadeOut('slow');
